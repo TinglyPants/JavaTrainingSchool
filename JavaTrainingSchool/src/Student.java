e953eef3487400f96eb34d9c8038d87d55a619e5
TinglyPants <chalkytangfastics@gmail.com>
2026-03-15 23:41:41 +0000

    Added Student

---
+++ JavaTrainingSchool/src/Student.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+
+public class Student extends Person{
+    private ArrayList<Integer> certificates;
+
+    public Student(String name, char gender, int age){
+        super(name, gender, age);
+        this.certificates = new ArrayList<>();
+    }
+
+    public void graduate(Subject subject){
+        this.certificates.add(subject.getID());
+    }
+
+    public ArrayList<Integer> getCertificates(){
+        return this.certificates;
+    }
+
+    public boolean hasCertificate(Subject subject){
+        return certificates.contains(subject.getID());
+    }
+}
