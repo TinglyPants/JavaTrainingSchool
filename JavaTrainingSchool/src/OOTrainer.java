3267a578cb9b37063e7c2f78f7dc33ce0a0ddda0
TinglyPants <chalkytangfastics@gmail.com>
2026-03-16 00:04:14 +0000

    Added OOTrainer

---
+++ JavaTrainingSchool/src/OOTrainer.java
@@ -0,0 +1,10 @@
+public class OOTrainer extends Instructor {
+    public OOTrainer(String name, char gender, int age) {
+        super(name, gender, age);
+    }
+
+    @Override
+    public boolean canTeach(Subject subject) {
+        return (subject.getSpecialism() == 1) || (subject.getSpecialism() == 2) || (subject.getSpecialism() == 3);
+    }
+}
